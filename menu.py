7c5673ad4182fce0825b5bb11835817a1c34dcc3
belen2985 <bellgimenez.bg@gmail.com>
2020-11-11 23:39:23 -0300

    Se agregó archivo menu.py y se completó hasta la opción 2

---
+++ menu.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+import sys
+from listaClientes import ListaClientes
+
+class Menu:
+    '''Mostrar un menú y responder a las opciones'''
+    def __init__(self):
+        self.lista_clientes = ListaClientes()
+        self.opciones = {
+            "1": self.mostrar_clientes,
+            "2": self.nuevo_cliente,
+            "0": self.salir
+        }
+
+    def mostrar_menu(self):
+        print("""
+Menú del sistema:
+1. Mostrar todos los clientes
+2. Alta de cliente
+0. Salir
+""")
+
+    def ejecutar(self):
+        '''Mostrar el menu y responder a las opciones.'''
+        while True:
+            self.mostrar_menu()
+            opcion = input("Ingresar una opción: ")
+            accion = self.opciones.get(opcion)
+            if accion:
+                accion()
+            else:
+                print("{0} no es una opción válida".format(opcion))
+
+    def mostrar_clientes(self, lista=None):
+        if lista == None:
+            lista = self.lista_clientes.lista
+        for cliente in lista:
+            print(cliente)
+            print("=====================================")
+    def nuevo_cliente(self):
+        tipo = "A"
+        while tipo not in ("C","c","P","p"):
+            tipo = input ("Ingrese el tipo de cliente: C:Corporativo / P:Particular: ")
+        nombre = input ("Ingrese el nombre: ")
+        if tipo in ("C","c"):
+            contacto = input("Ingrese el nombre del contacto: ")
+            tc = input("Ingrese el teléfono del contacto: ")
+        else:
+            apellido = input("Ingrese el apellido: ") 
+        tel = input("Ingrese el teléfono: ")
+        mail = input("Ingrese el e-mail: ")
+        if tipo in ("C","c"):
+            c = self.lista_clientes.nuevo_cliente_corporativo(nombre, contacto, tc, tel, mail)
+        else:
+            c = self.lista_clientes.nuevo_cliente_particular(nombre, apellido, tel, mail)
+        if c is None:
+            print("Error al cargar el cliente")
+        else:
+            print("Cliente cargado con éxito")       
+
+    def salir(self):
+        print("Gracias por utilizar el sistema.")
+        sys.exit(0)
+
+#Esta parte del código está fuera de la clase Menu.
+#Si este archivo es el programa principal (es decir, si no ha sido importado
+#desde otro módulo, sino ejecutado directamente), entonces llamamos al método
+# ejecutar().
+if __name__ == "__main__":
+    Menu().ejecutar()
